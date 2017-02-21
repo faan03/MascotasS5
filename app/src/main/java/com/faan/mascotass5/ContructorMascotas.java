e5195e03ca3d2d90b40523fa9b71088c26df89fd
faan03 <faan03@gmail.com>
2017-02-21 08:53:14 -0500

    ok

--- app/src/main/java/com/faan/mascotass5/ContructorMascotas.java
+++ app/src/main/java/com/faan/mascotass5/ContructorMascotas.java
@@ -14,7 +14,7 @@ public class ContructorMascotas {
         this.context = context;
     }
 
-    public ArrayList<Mascota> obtenerMascotasFav5() {
+    public ArrayList<Mascota> obtenerCincoMascotas() {
         BaseDeDatos db = new BaseDeDatos(context);
         return db.obtenerCincoMascotas();
     }
